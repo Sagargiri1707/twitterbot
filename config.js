d245ac10102363a2ab46c2d573d90d23f27c45ab
Sagar Giri <sagargiri8846@gmail.com>
2020-10-10 10:16:30 +0530

    taiwan

---
+++ config.js
@@ -0,0 +1,15 @@
+//config.js
+/** TWITTER APP CONFIGURATION
+ * consumer_key
+ * consumer_secret
+ * access_token
+ * access_token_secret
+ */
+module.exports = {
+    consumer_key: 'ljWqpBbc8uTLoucdXsVYTXvXm',  
+    consumer_secret: 'gwqkprhjtK4DYPq3df2JedenUvlAgbcr2PSXjCu92HnqB20gxF',
+    access_token: '893418621661323264-lRrUphlDZlgvqrIcVcwmIoSPwFwOgyc',  
+    access_token_secret:'NofmjpBmGdd0BDhVZkPctUUoj4nKP2uTGiORc4rKfxAJQ'
+}
+  
+//access_token: ''FY5onjyt9Yz6RUwWsSIuY6OPi    j1hdyeTyQ4Fy0lSlmcRJO6vgwBVEgbWpWqIcqtYlJquTHSbEgo
