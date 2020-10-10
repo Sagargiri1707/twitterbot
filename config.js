05d921ac2126c457353c5e994563fbdad146cb3d
Sagar Kumar Giri <59120491+asd12-coder@users.noreply.github.com>
2020-10-10 11:24:40 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,10 +6,10 @@
  * access_token_secret
  */
 module.exports = {
-    consumer_key: 'ljWqpBbc8uTLoucdXsVYTXvXm',  
-    consumer_secret: 'gwqkprhjtK4DYPq3df2JedenUvlAgbcr2PSXjCu92HnqB20gxF',
-    access_token: '893418621661323264-lRrUphlDZlgvqrIcVcwmIoSPwFwOgyc',  
-    access_token_secret:'NofmjpBmGdd0BDhVZkPctUUoj4nKP2uTGiORc4rKfxAJQ'
+    consumer_key: '',  
+    consumer_secret: '',
+    access_token: '',  
+    access_token_secret:''
 }
   
 //access_token: ''FY5onjyt9Yz6RUwWsSIuY6OPi    j1hdyeTyQ4Fy0lSlmcRJO6vgwBVEgbWpWqIcqtYlJquTHSbEgo
