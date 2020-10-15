e86abe1a04b9fd9a226e955781919ec01a1a2459
Sagar Giri <sagargiri8846@gmail.com>
2020-10-15 19:14:41 +0530

    rcb vs kxip

--- bot.js
+++ bot.js
@@ -11,7 +11,7 @@ var Twitter = new twit(config);
 // find latest tweet according the query 'q' in params
 var retweet = function() {
   var params = {
-        q: '#RCBvsKKR',  
+        q: '#RCBvsKXIP',  
         lang: 'en'
     }
     Twitter.get('search/tweets', params, function(err, data) {
@@ -50,7 +50,7 @@ setInterval(retweet,10000);
 // find a random tweet and 'favorite' it
 var favoriteTweet = function(){
   var params = {
-    q: '#RCBvsKKR',  // REQUIRED
+    q: '#RCBvsKXIP',  // REQUIRED
     result_type: "recent",
     lang: 'en',
       count:100
--- config.js
+++ config.js
@@ -6,10 +6,10 @@
  * access_token_secret
  */
 module.exports = {
-    consumer_key: '',  
-    consumer_secret: '',
-    access_token: '',  
-    access_token_secret:''
+    consumer_key: 'h0Uh0HztkKewBGPCOXZJUVmpH',  
+    consumer_secret: 'hthkMaLze8mRJ1pYEQ7EHl1QE4Y5lthp1sVyzUY35jnicOWrw3',
+    access_token: '893418621661323264-RNkbTL5XpspuQdPJpZGAe4KMtCI2P2s',  
+    access_token_secret:'RahE9RbYz57elq1LiiLju9QLDYs8VqLR2GMWBNC0I6M9N'
 }
   
 //access_token: ''FY5onjyt9Yz6RUwWsSIuY6OPi    j1hdyeTyQ4Fy0lSlmcRJO6vgwBVEgbWpWqIcqtYlJquTHSbEgo
